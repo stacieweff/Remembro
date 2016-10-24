42c01ef292f611fec617f8210d8574af36744fdd
kennedyb <brian.kennedy@wwt.com>
2016-10-24 16:54:40 -0500

    setting up and working now

--- app/scripts/controllers/titlebar.js
+++ app/scripts/controllers/titlebar.js
@@ -9,6 +9,10 @@
  */
 angular.module('remembroApp')
   .controller('TitlebarCtrl', function ($scope, Auth, $location, $timeout) {
+      $scope.user = firebase.auth().currentUser;
+      $scope.userPanelShow = false;
+
+
       $scope.oauthLogin = function(provider) {
           $scope.err = null;
           var auth = firebase.auth();
@@ -16,34 +20,51 @@ angular.module('remembroApp')
           var provider = new firebase.auth.FacebookAuthProvider();
           auth.signInWithPopup(provider).then(function(result) {
               // User signed in!
-              console.log(result);
               $timeout(function() {
                   $scope.user = result.user;
+                  $scope.createUserInRemembro();
               });
           }).catch(function(error) {
               showError(error);
           });
       };
 
-      $scope.checkIfAuthed = function() {
-          console.log(Auth.$getAuth());
-          if(!Auth.$getAuth()) {
-
-          }
-      }
-
-      $scope.oauthLogout = function() {
-          firebase.auth().signOut().then(function() {
-              $timeout(function() {
-                  $scope.user = null;
-              });
-          }, function(error) {
-              showError(error)
-          });
+      $scope.checkIfUserInRemembro = function() {
+          //pseudo code
+          //Do a GET to your Remembro users node where displayName is $scope.user.displayName
+          //if it comes back null - you know the user is brand new.  If not, they are returning.
+          //if new do this
+          $scope.createUserInRemembro();
       };
 
-      $scope.showAccountInfo = function() {
-          $scope.isShowingInfo = !$scope.isShowingInfo;
+      $scope.createUserInRemembro = function() {
+        //POST to add a new object to the users node for this new user - with name, email, etc...
+      };
+
+      $scope.logout = function() {
+        firebase.auth().signOut().then(function() {
+            console.log('we signed out - show some ui message');
+            $scope.checkIfAuthed();
+        }, function(error) {
+            console.log('signout error, show a message, log it');
+        });
+      };
+
+      $scope.toggleUserPanel = function() {
+          $scope.userPanelShow = !$scope.userPanelShow;
+      };
+
+      $scope.checkIfAuthed = function() {
+          console.log(firebase.auth());
+          if (firebase.auth().currentUser) {
+              $scope.user = firebase.auth().currentUser;
+          } else {
+              console.log('got in here');
+              $timeout(function(){
+                  $scope.user = firebase.auth().currentUser;
+                  $scope.userPanelShow = false;
+              });
+          }
       };
 
       function init() {
